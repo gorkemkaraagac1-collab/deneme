7b0d25c157df718b07441cf0bb84a4f1913217c1
gorkemkaraagac1-collab <gorkemkaraagac1@gmail.com>
2026-08-23 21:23:24 +0300

    Refactor import of TextEncoder and TextDecoder

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,7 @@
-const { TextEncoder, TextDecoder } = require("util");
+const {
+  TextEncoder,
+  TextDecoder
+} = require("util");
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
