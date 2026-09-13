291a0997ede519957ca60096c0ab64eb3dda23dd
gorkemkaraagac1-collab <gorkemkaraagac1@gmail.com>
2026-09-13 15:58:50 +0300

    fix: send HttpOnly session with all API requests (#226)

--- js/tfrs16-engine.js
+++ js/tfrs16-engine.js
@@ -345,13 +345,9 @@ window.fetch = (input, init = {}) => {
 
   async function tfrs16ApiFetch(path, options = {}) {
     const token = tfrs16GetToken();
-    if (!token) {
-      const err = new Error("Oturum bulunamadı. Lütfen tekrar giriş yapın.");
-      err.code = "NO_TOKEN";
-      throw err;
-    }
     const res = await fetch(`${TFRS16_API_BASE}${path}`, {
       ...options,
+    credentials: "include",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
